9f4650e2b2f46ceea45ac865bf3dbd7010017d17
wjw <2876645134@qq.com>
2021-10-17 04:32:22 +0800

    third commit

--- src/main/java/com/at/git/GitTest.java
+++ src/main/java/com/at/git/GitTest.java
@@ -3,6 +3,7 @@ package com.at.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello git");
-        System.out.println("Hello git");
+        System.out.println("Hello git2");
+        System.out.println("Hello git3");
     }
 }
